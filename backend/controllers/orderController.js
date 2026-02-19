a009c0f6f8a2ac4cd15d1d8100d92d05e3287eb1
Rajib Subedi <subedirajib59@gmail.com>
2026-02-19 17:47:08 +0545

    changing the order update status

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 //placing userorder for frontend
 const placeOrder = async(req, res) =>{
-    const frontend_url = "http://localhost:5173";
+    const frontend_url = "http://localhost:5174";
     try {
         const newOrder = new orderModel({
             userId: req.body.userId,
@@ -42,6 +42,20 @@ const listOrders= async (req,res) =>{
         res.json({success:false,message:"Error"})
     }
 }
-export {placeOrder,userOrders,listOrders};
+
+// api for updating order status
+const updateStatus = async (req,res) =>{
+    try {
+        await orderModel.findByIdAndUpdate(req.body.orderId,{status:req.body.status});
+        res.json({success:true,message:"Status Updated"})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"});
+    }
+}
+
+
+
+export {placeOrder,userOrders,listOrders,updateStatus};
 
 
