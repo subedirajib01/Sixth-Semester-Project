d440454ae589faa897ba4d0f9df720237e64e18c
Rajib Subedi <subedirajib59@gmail.com>
2026-05-12 20:23:55 +0545

    final changes

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ return (
     <div className="header-contents">
         <h2>Discover your favourite food here</h2>
         <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our passion is to bring you flavors that satisfy your cravings and elevate your dining experience—one delicious meal at a time. </p>
-        <button>ExploreMenu</button>
+        <button >ExploreMenu</button>
     </div>
     </div>
 )
